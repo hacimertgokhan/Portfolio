8ef12958ae3725f413063b699703e80a6ba6d5e9
Mert Gökhan <hacimertgokhan@gmail.com>
2023-09-09 08:51:44 +0300

    0.005
    Added mobile and other platform support main page, created some new animations for quality and added more soft things.

--- assets/javascript/updater.js
+++ assets/javascript/updater.js
@@ -2,16 +2,14 @@ consoleText(['Java Developer', 'Web Developer', 'UI/UX Designer'], 'text',['#366
 
 function consoleText(words, id, colors) {
   if (colors === undefined) colors = ['#fff'];
-  var visible = true;
-  var con = document.getElementById('console');
   var letterCount = 1;
   var x = 1;
   var waiting = false;
   var target = document.getElementById(id)
   target.setAttribute('style', 'color:' + colors[0])
   window.setInterval(function() {
-
     if (letterCount === 0 && waiting === false) {
+      
       waiting = true;
       target.innerHTML = words[0].substring(0, letterCount)
       window.setTimeout(function() {
@@ -24,6 +22,7 @@ function consoleText(words, id, colors) {
         letterCount += x;
         waiting = false;
       }, 1000)
+
     } else if (letterCount === words[0].length + 1 && waiting === false) {
       waiting = true;
       window.setTimeout(function() {
@@ -31,20 +30,11 @@ function consoleText(words, id, colors) {
         letterCount += x;
         waiting = false;
       }, 1000)
+
     } else if (waiting === false) {
       target.innerHTML = words[0].substring(0, letterCount)
       letterCount += x;
     }
+
   }, 120)
-  window.setInterval(function() {
-    if (visible === true) {
-      con.className = 'console-underscore hidden'
-      visible = false;
-
-    } else {
-      con.className = 'console-underscore'
-
-      visible = true;
-    }
-  }, 400)
 }
